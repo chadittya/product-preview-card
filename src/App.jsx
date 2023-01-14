5acb4f22301f9e0a98c75a3b8605bb2876b8403a
chadittya <chadittya@gmail.com>
2023-01-15 04:21:48 +0700

    improve HTML dengan wrap all content dengan tag main. menambahkan h1 pada judul product. menggunakan tag picture untuk responsive image product dan mengunakan del serta clas sr only untuk improve diskon untuk screen reader

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,23 @@ import { AiOutlineShoppingCart } from "react-icons/ai";
 
 function App() {
   return (
-    <>
+    <main>
       <div className="flex flex-row justify-center items-center h-screen box-border">
         <div className="bg-bgWhite rounded-xl md:w-[600px] mobile:w-[345px] md:h-auto mobile:h-[611px]">
           <div className="flex md:flex-row mobile:flex-col">
             <div className="md:w-1/2">
-              <img
-                src={bgImageProductDesktop}
-                alt="image-product-desktop"
-                className="md:h-full rounded-l-xl mobile:hidden md:block"
-              />
-              <img
-                src={bgImageProductMobile}
-                alt="image-product-desktop"
-                className="md:hidden rounded-t-xl"
-              />
+              <picture>
+                <source
+                  media="(max-width: 720px)"
+                  srcSet={bgImageProductMobile}
+                />
+                <img srcSet={bgImageProductDesktop} alt="product 1" />
+              </picture>
             </div>
             <div className="px-6 md:w-1/2 py-6">
-              <p className="font-Montserrat text-gray-400 tracking-[4px] text-sm mobile:text-[13px]">
+              <h1 className="font-Montserrat text-gray-400 tracking-[4px] text-sm mobile:text-[13px]">
                 PERFUME
-              </p>
+              </h1>
               <p className="font-Fraunces font-bold text-4xl text-[37px] py-5 w-auto mobile:text-[32px]">
                 Gabriele Essense Eau De Parfum
               </p>
@@ -35,19 +32,22 @@ function App() {
                 <p className="font-Fraunces font-bold text-bgDarkCyan text-4xl mobile:text-3xl">
                   $149.99
                 </p>
-                <p className="flex items-center font-Montserrat text-gray-400 line-through text-sm">
-                  $169.99
+                <p className="flex items-center font-Montserrat text-gray-400 text-sm">
+                  <del>
+                    <span className="sr-only">Old price:</span>
+                    $169.99
+                  </del>
                 </p>
               </div>
               <button className="flex items-center justify-center bg-bgDarkCyan rounded-lg text-center w-full h-12 gap-2 text-white font-Montserrat font-semibold hover:bg-bgDarkGrayishBlue mobile:h10 mobile:text-sm">
                 <AiOutlineShoppingCart />
-                <p>Add to Cart</p>
+                Add to Cart
               </button>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </main>
   );
 }
 
